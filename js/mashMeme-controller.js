d55c163d3de806536338f34f892707bd917ec4b6
osho80 <omh1980@gmail.com>
2020-04-10 12:12:15 +0300

    my second commit 12:12

--- js/mashMeme-controller.js
+++ js/mashMeme-controller.js
@@ -34,9 +34,46 @@ function renderGallery() {
 
 function onSetEditor(img) {
     gElGallery.style.display = 'none';
-    gElEditor.style.display = 'block'
+    gElEditor.style.display = 'flex';
+    // gElGallery.style.visibility = 'hidden';
+    // gElEditor.style.visibility = 'visible';
+    
+    
     gCtx.drawImage(img, 0, 0, gCanvas.width, gCanvas.height);
     // console.log(img);
     
 }
 
+function onDrawText(txt) {
+    console.log(txt);
+    //selected Img, LineIdx
+    // gMeme.selectedImgId
+    gMeme.lines[0].text = txt;
+    // render
+    drawText (gMeme);
+}
+
+function drawText(meme) {
+    gCtx.beginPath();
+    // gCtx.moveTo(10, 10);
+    
+    // gCtx.lineWidth = '2'
+    gCtx.strokeStyle = gMeme.lines[0].color
+    gCtx.fillStyle = 'white'
+    gCtx.font = gMeme.lines[0].size + 'px' + ' ' + gMeme.lines[0].font;
+    
+    gCtx.textAlign = gMeme.lines[0].align
+    var memeTxt = gMeme.lines[0].text;
+    gCtx.fillText(memeTxt, 10, 60);
+    gCtx.closePath();
+    gCtx.strokeText(memeTxt, 10, 60);
+    // (text, x, y)
+}
+
+function onIncreaseFont() {
+    gMeme.lines[0].size++
+}
+
+function onDecreaseFont() {
+    gMeme.lines[0].size--
+}
